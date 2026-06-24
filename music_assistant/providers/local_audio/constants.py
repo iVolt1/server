12f3f0b3181bc8539975c2d489e8b5cee845569c
iVolt1 <brady725@gmail.com>
2026-06-24 16:58:05 -0500

    0.003162

--- music_assistant/providers/local_audio/constants.py
+++ music_assistant/providers/local_audio/constants.py
@@ -54,7 +54,8 @@ DEFAULT_BUFFER_FRAMES = 1024  # sounddevice blocksize for macOS/ALSA PortAudio o
 # the same regardless of which volume-control mode is active.
 # _TAPER_A: Final = 0.001  # 10**(-60/20) -- amplitude at the -60dB reference point
 # 40dB range (better for receiver/outdoor speakers):
-_TAPER_A: Final = 0.01  # 10**(-40/20)
+_TAPER_A: Final = 0.03162  # 10**(-30/20)
+# _TAPER_A: Final = 0.01  # 10**(-40/20)
 _TAPER_B: Final = math.log(1.0 / _TAPER_A)  # ~6.908, gives 60dB range over x in [0, 1]
 _TAPER_ROLLOFF_X: Final = 0.10  # below 10% slider, linear ramp to true silence
 
